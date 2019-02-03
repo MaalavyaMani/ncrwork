597787f6cb64be217f9adc1495598fbad927e010
Maalavya <maalavyamani@gmail.com>
2019-02-03 15:53:48 +0530

    add rotate right

---
+++ rightRotate/rightRotate/rightRotate.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#define INT_BITS 32
+
+
+int rotate_right(int n, int d)
+{
+	
+	return (n >> d) | (n << (INT_BITS - d));
+}
+
+int main()
+{
+	int n, d;
+	printf("enter the number and the shift \n");
+	scanf_s("%d %d", &n, &d);
+	
+	printf("\n Right Rotation of %d by %d is ", n, d);
+	printf("%d", rotate_right(n, d));
+	getch();
+}
