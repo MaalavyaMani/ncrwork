8145092df2f942208de016a6f2f1553f3f3b7d7f
Maalavya <maalavyamani@gmail.com>
2019-02-03 17:09:21 +0530

    add max, min values

--- positiveSum/positiveSum/positiveSum.c
+++ positiveSum/positiveSum/positiveSum.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int num, sum, avg, count=0 ,total;
-
+	int num, sum=0, avg, count = 0, max=0, min=0;
 	printf("Enter positive numbers:\n");
-	sum = 0;
 	scanf_s("%d", &num);
-	while (num > 0)
-	{
+	while(num>0) {
+		
+		if (num > max) max = num;
+		if (num < min) min = num;
 		sum += num;
 		scanf_s("%d", &num);
 		count++;
-	}
-	total = count;
-	avg = sum / total;
-	printf("%d %d ",total, avg );
+	} 
+
+	avg = sum / count;
+	printf("%d %d %d %d ", count, max, min, avg);
 	getch();
 
 	return 0;
