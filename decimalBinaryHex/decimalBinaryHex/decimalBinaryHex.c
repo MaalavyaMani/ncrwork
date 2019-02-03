a345e53322ce81a65890447f3862887d8f68edf5
Maalavya <maalavyamani@gmail.com>
2019-02-03 19:26:43 +0530

    add binary and hex conversion

---
+++ decimalBinaryHex/decimalBinaryHex/decimalBinaryHex.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int itob(int n, int s);
+int itoh(int n, int s);
+int main() {
+	long int n, b, c, h, s = 0;
+	scanf_s("%ld", &n);
+	b = itob(n, s);
+	printf("\n Binary=%ld", b);
+	c = itoh(n, s);
+	getch();
+	return 0;
+}
+int itob(int n, int s) {
+	int rem, base = 1;
+	while (n > 0) {
+		rem = n % 2;
+		s = s + rem * base;
+		n = n / 2;
+		base = base * 10;
+	}
+	return s;
+}
+int itoh(int n, int s) {
+	printf("\n HexaDecimal =%x", n);
+	return n;
+}
---
+++ invertPonwards/invertPonwards/invertPonwards.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+	int X, P, N, RESULT;
+	int invertponwards(int, int, int);
+	printf("Enter integer, start position, length to invert: \n");
+	scanf_s("%d %d %d", &X, &P, &N);
+	RESULT = invertponwards(X, P, N);
+	printf("\nINVERTED INTEGER IN DECIMAL FORM IS \t%d", RESULT);
+	return 0;
+	getch();
+}
+
+int invertponwards(int X, int P, int N)
+{
+	int I, Y, LIMIT, LEN, BIN[15];
+	LEN = 0;
+	while (X > 0)
+	{
+		BIN[LEN] = X % 2;
+		X = X / 2;
+		LEN++;
+	};
+
+	for (I = LEN - 1; I > -1; I--)
+	{
+		printf("%d ", BIN[I]);
+	}
+	printf("\n");
+	LIMIT = P + N;
+	if (LIMIT < LEN)
+	{
+		for (I = P - 1; I < P + N - 1; I++)
+		{
+			if (BIN[I] == 0)
+				BIN[I] = 1;
+			else
+				BIN[I] = 0;
+		}
+	}
+	else
+	{
+		printf("YOU HAVE ENTERED LARGE VALUE FOR P AND N");
+	}
+
+	printf("INVERTED INTEGER IN BINARY FORM IS \t");
+	for (I = LEN - 1; I > -1; I--)
+	{
+		printf("%d ", BIN[I]);
+	}
+	Y = 0;
+	for (I = LEN - 1; I >= 0; I--)
+	{
+		Y = Y * 2 + BIN[I];
+	}
+	return Y;
+}
