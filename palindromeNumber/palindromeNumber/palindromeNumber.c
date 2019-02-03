07476b08a340df3302bc95e7094b77be8d73950b
Maalavya <maalavyamani@gmail.com>
2019-02-03 10:56:36 +0530

    add palindrome

---
+++ palindromeNumber/palindromeNumber/palindromeNumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int number, reverse = 0, temp, num;
+
+	printf("Enter a number\n");
+	scanf_s("%d", &number);
+
+	temp = number;
+
+	while (temp != 0)
+	{
+		reverse = reverse * 10;
+		reverse = reverse + temp % 10;
+		temp = temp / 10;
+	}
+
+	if (number == reverse)
+		printf("%d is a palindrome number\n", number);
+	else
+		printf("%d is not a palindrome number\n", number);
+	
+	scanf_s("%d", &num);
+
+	return 0;
+}
