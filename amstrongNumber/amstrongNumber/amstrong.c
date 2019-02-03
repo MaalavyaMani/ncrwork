72cfa9e886f7763f3f30a5f8f3c742a22af643c9
Maalavya <maalavyamani@gmail.com>
2019-02-03 10:38:16 +0530

    amstrongNumber

---
+++ amstrongNumber/amstrongNumber/amstrong.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+	int number, originalNumber, remainder, result = 0,var;
+
+	printf("Enter a three digit integer: \n ");
+	scanf_s("%d", &number);
+
+	originalNumber = number;
+
+	while (originalNumber != 0)
+	{
+		remainder = originalNumber % 10;
+		result += remainder * remainder*remainder;
+		originalNumber /= 10;
+	}
+
+	if (result == number)
+		printf("\n %d is an Armstrong number", number);
+	    
+	else
+		printf("\n %d is not an Armstrong number", number);
+
+	scanf_s("%d", &var);
+	    
+	return 0;
+}
