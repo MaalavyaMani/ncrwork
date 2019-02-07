1b63060c8c371f8daa3f0697b85127593bb9059d
Maalavya <maalavyamani@gmail.com>
2019-02-07 21:03:33 +0530

    add colour table

--- colorTable/colorTable/colorTable.c
+++ colorTable/colorTable/colorTable.c
@@ -5,7 +5,7 @@ int main() {
 	printf("the value of color+2 %d \n", color+2);
 	printf("the value of *color %s \n", *color);
 	printf("the value of *(color+2) %s \n", *(color+2));
-	printf("the value of color[5] and *(color+5) %s and %s ", color[5], *(color + 5));
+	printf("the value of color[5] and *(color+5) %s and %s ", color[5],*(color + 5));
 
 	getch();
 	return 0;
