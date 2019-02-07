fb25e4d231b81094c1e47457ae25820f1b89ef68
Maalavya <maalavyamani@gmail.com>
2019-02-07 22:52:13 +0530

    add rotations of string

---
+++ stringRotations/stringRotations/stringRotations.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+
+
+int main()
+{
+	char a[100];
+	int i, j;
+	printf("Enter a word: ");
+	gets(a);
+
+	for (i = 0; a[i] != 0; i++)
+	{
+		for (j = i; a[j] != 0; j++)
+			printf("%c", a[j]);
+
+		for (j = 0; j < i; j++)
+			printf("%c", a[j]);
+		printf("\n");
+	}
+	getch();
+	return 0;
+}
